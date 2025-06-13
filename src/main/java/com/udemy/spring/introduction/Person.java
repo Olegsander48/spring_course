8a55db186d074d4c7bbf1216b4842e1c92b8ead6
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-06-13 09:20:24 +0300

    dependency injection using setter

--- src/main/java/com/udemy/spring/introduction/Person.java
+++ src/main/java/com/udemy/spring/introduction/Person.java
@@ -3,12 +3,15 @@ package com.udemy.spring.introduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
-        this.pet = pet;
+    public Person() {
     }
 
     public void callYourPet() {
         System.out.println("Hello, my lovely pet!");
         pet.say();
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
     }
 }
