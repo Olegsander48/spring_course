43c4a0d667ebe5e4c5777e57075b4a08fc39ed30
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-07-10 16:59:43 +0300

    example of java servlet

---
+++ src/main/java/com/udemy/spring/servlets/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.udemy.spring.servlets;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println(">>> HelloServlet initialized <<<");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello from Servlet!</h1>");
+    }
+}
