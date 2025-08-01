58838532630123101f90a9deb04d7fd7468f1f54
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-08-01 10:25:27 +0300

    write requests to database using HQL

--- src/main/java/com/udemy/spring/hibernate/Main.java
+++ src/main/java/com/udemy/spring/hibernate/Main.java
@@ -1,9 +1,12 @@
 package com.udemy.spring.hibernate;
 
 import com.udemy.spring.hibernate.entity.Employee;
+import jakarta.persistence.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,15 +14,51 @@ public class Main {
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory();
+        Main main = new Main();
+        main.getAll(factory).forEach(System.out::println);
+        System.out.println(main.getByName("Sasha", factory));
+        factory.close();
+    }
+
+    private void getEmployeeById(int id, SessionFactory sessionFactory) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.get(Employee.class, id);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    private List<Employee> getAll(SessionFactory sessionFactory) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from Employee");
+        List<Employee> result = query.getResultList();
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    private Employee getByName(String name, SessionFactory sessionFactory) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from Employee where name = :name")
+                .setParameter("name", name);
+        Employee result = (Employee) query.getResultList().get(0);
+        session.getTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    private void addNewEmployee(SessionFactory sessionFactory) {
         try {
-            Session session = factory.getCurrentSession();
-            Employee employee = new Employee("Sasha", "Petrov", "IT", 1000);
+            Session session = sessionFactory.getCurrentSession();
+            Employee employee = new Employee("Vladimir", "Vaskob", "HT", 5000);
             session.beginTransaction();
             session.persist(employee);
             session.getTransaction().commit();
             session.close();
-        } finally {
-            factory.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
